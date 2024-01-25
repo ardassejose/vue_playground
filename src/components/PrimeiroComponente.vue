99dea252bde8970aed825b856d7c42c4380aa1fa
José Víctor Ardasse <josevictorps3lima@gmail.com>
2024-01-25 17:30:41 -0300

    feat: independent components

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,16 +3,23 @@
     <h1>Olá Vue.js!</h1>
     <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
     <MultiplosEventos />
+    <Reutilizacao />
+    <Reutilizacao />
+    <Reutilizacao />
+    <Reutilizacao />
+    <Reutilizacao />
   </div>
 </template>
 
 <script defer>
   import MultiplosEventos from './MultiplosEventos.vue'
+  import Reutilizacao from './Reutilizacao.vue';
 
   export default {
     name: "PrimeiroComponente",
     components: {
-      MultiplosEventos
+      MultiplosEventos,
+      Reutilizacao
     },
     data() {
       return {
