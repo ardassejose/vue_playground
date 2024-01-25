e6fe30f1d92e4f2fa74febb8e558a3cc54780b75
José Víctor Ardasse <josevictorps3lima@gmail.com>
2024-01-25 17:24:44 -0300

    feat: multiple target event

---
+++ src/components/MultiplosEventos.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button @click="primeiro(txt1, $event), segundo(txt2,$event), terceiro($event)">Ativar múltiplos eventos</button>
+    <p>{{ multiplosEventos }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultiplosEventos",
+  data() {
+    return {
+      multiplosEventos: "",
+      txt1: "Primeiro text",
+      txt2: "Segundo text"
+    }
+  },
+  methods: {
+    primeiro(txt, e) {
+      this.multiplosEventos = txt
+    },
+    segundo(txt, e) {
+      setTimeout(() => this.multiplosEventos = txt, 500)
+    },
+    terceiro() {
+      setTimeout(() => this.multiplosEventos = "", 1500)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    margin: 100px;
+  }
+</style>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,12 +2,18 @@
   <div>
     <h1>Olá Vue.js!</h1>
     <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    <MultiplosEventos />
   </div>
 </template>
 
 <script defer>
+  import MultiplosEventos from './MultiplosEventos.vue'
+
   export default {
     name: "PrimeiroComponente",
+    components: {
+      MultiplosEventos
+    },
     data() {
       return {
         nome: "Víctor",
