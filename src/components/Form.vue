2c18082f5453feb8d25e9ea295f09c0f7af01666
José Víctor Ardasse <josevictorps3lima@gmail.com>
2024-01-24 22:23:31 -0300

    Formulário

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,20 +1,46 @@
 <template>
-  <form action="" style="display: flex; flex-direction: column; max-width: fit-content;">
-    <InputText />
-    <InputText />
+  <form
+    action=""
+    @submit="enviarFormulario($event)"
+    style="display: flex; flex-direction: column; max-width: fit-content"
+  >
+    <div>
+      <input type="text" v-model="name" />
+    </div>
+    <div>
+      <input type="text" v-model="email" />
+    </div>
     <Submit />
   </form>
 </template>
 
 <script>
-import InputText from './form/InputText.vue';
-import Submit from './form/Submit.vue';
+import InputText from "./form/InputText.vue";
+import Submit from "./form/Submit.vue";
 
-  export default {
-    name: 'Form',
-    components: {
-      InputText,
-      Submit
-    }
-  }
+export default {
+  name: "Form",
+  components: {
+    InputText,
+    Submit,
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  methods: {
+    enviarFormulario(e) {
+
+      e.preventDefault();
+
+      const name = this.name;
+      const email = this.email;
+
+      window.alert("Form enviado!");
+      window.alert(`O nome é ${name} e o email: ${email}`);
+    },
+  },
+};
 </script>
